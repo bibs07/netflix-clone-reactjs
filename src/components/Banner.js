e1ff64e66361910827e651aeb3eab6bd08cb930b
bibs07 <52453292+bibs07@users.noreply.github.com>
2021-08-23 02:55:05 +1000

    feat: cleared console logs

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,8 +18,6 @@ export const Banner = () => {
     fetchData();
   }, []);
 
-  console.log(movie);
-
   function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
